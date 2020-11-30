348f3a1972ba76a6ffc84722aed65c1c081bbed8
chunlinwang <chunlinwang1983@gmail.com>
2020-11-30 23:57:54 +0100

    add api with readFile

--- webserver/simple-routing.js
+++ webserver/simple-routing.js
@@ -1,10 +1,16 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
 
 const server = http.createServer((
     req, res
 ) => {
     const path = req.url;
-    switch (path) {
+
+    const urlObj = url.parse(req.url);
+
+    switch (urlObj.pathname) {
         case '/':
         case '/index.html':
             res.writeHead(200, {
@@ -18,12 +24,29 @@ const server = http.createServer((
             });
             res.end('<H1>Products</H1>')
             break;
+        case '/api':
+            fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, buffer) => {
+                const queryObj = qs.parse(urlObj.query);
+                const objs = JSON.parse(buffer);
+                if (err) {
+                    res.writeHead(500, {
+                        'content-type': 'application/json',
+                    });
+                    res.end({error: err.message});
+                }
+
+                const result = (queryObj.id === undefined) ? [] : objs.filter((obj) => obj.id == queryObj.id);
+
+                res.end(JSON.stringify(result));
+            });
+            break;
         default:
             res.writeHead(404, {
                 'content-type': 'text/html',
                 'my-header-error': 'error404'
             });
             res.end('<H1>Page not found!</H1>')
+            break;
     }
 });
 
